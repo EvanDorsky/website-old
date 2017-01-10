2b32096da98b7f622336e2e919ead8878a563ad6
Evan Dorsky <evan.dorsky@icloud.com>
2017-01-09 22:39:45 -0800

    Need to justify this feature

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     $('.cat').click(function() {
         var cat = $(this).attr('cat')
         var images = categories[cat].images
-        
+
         $('.photo-box').empty()
         for (i in images) {
             var img = images[i]
