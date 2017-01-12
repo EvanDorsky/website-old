deeefaabb2a09b54ed031aea5141f97d151d77f8
Evan Dorsky <edorsky@apple.com>
2017-01-12 09:17:57 -0800

    Semi-working img make/clean

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
-index.html : pug/index.pug pug/coolstuff.json stylesheets/style.css
-	cp node_modules/zepto/dist/zepto.min.js js
-	pug pug/index.pug -O pug/coolstuff.json -o .
+index.html : pug/index.pug js/coolstuff.json stylesheets/style.css
+	pug pug/index.pug -O js/coolstuff.json -o .
 
-stylesheets/style.css : stylus/*
+stylesheets/style.css: stylus/*
 	stylus stylus/style.styl -o stylesheets
+
+.PHONY:clean img
+
+img:
+	python process_img.py
+
+clean:
+	rm -f stylesheets/*
+	rm -f index.html
+	rm -f **/**/*_*.jpg
+	rm -f **/*_*.jpg
---
+++ process_img.py
@@ -0,0 +1,38 @@
+import sys, os
+from subprocess import call
+
+def thumb(path, height):
+    dirname = os.path.dirname(path)
+    filename = os.path.basename(path)
+    path = os.path.join(path)
+    path_tn = os.path.join(dirname,
+        filename.split('.')[0]+'_tn.jpg')
+    call(['convert', path, '-resize', str(height), path_tn])
+    return path_tn
+
+def gray(path):
+    dirname = os.path.dirname(path)
+    filename = os.path.basename(path)
+    call(['convert',
+        path,
+        '-grayscale',
+        'rec709luma',
+        os.path.join(dirname,
+            filename.split('.')[0]+'_gray.jpg')
+        ])
+
+def process(path):
+    path_tn = thumb(path, 360)
+    gray(path)
+    gray(path_tn)
+
+def process_files():
+    for dirname, dirnames, filenames in os.walk('img'):
+        for filename in filenames:
+            if '.jpg' in filename:
+                path = os.path.join(dirname, filename)
+                process(path)
+
+### main
+
+process_files()
