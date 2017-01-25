3cfdfa07613d7c1daf579f6a6ccf7a6cc49d2811
Evan Dorsky <evan.dorsky@icloud.com>
2017-01-24 20:45:02 -0800

    Basic picking

--- js/main.js
+++ js/main.js
@@ -3,7 +3,24 @@
 })()
 
 function init() {
-    $('.cat-img').click(function() {
-        console.log($('.cat-details').find(['name="yowona"']))
-    })
+    $('.cat-img').click(handleImgClick)
+}
+
+function handleImgClick() {
+    var name = $(this).attr('name')
+    var catDetail = $('.cat-details[name='+name+']')
+    var catDetails = $('.cat-details')
+
+    var colorImg = $('.cat-img[name='+name+']').find('img.color')
+    var colorImgs = $('.cat-img').find('img.color')
+
+    var wasHidden = catDetail.css('display') == 'none'
+
+    colorImgs.removeClass('active')
+    catDetails.hide()
+
+    if (wasHidden) {
+        colorImg.addClass('active')
+        catDetail.show()
+    }
 }
