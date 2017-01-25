1136a9e79fdbbeeb1e3414ea9ddab0fe606f1a65
Evan Dorsky <evan.dorsky@icloud.com>
2017-01-24 21:03:35 -0800

    Scroll to selected image

--- js/main.js
+++ js/main.js
@@ -8,18 +8,30 @@ function init() {
 
 function handleImgClick() {
     var name = $(this).attr('name')
+    var photoBox = $(this).parent()
+
     var catDetail = $('.cat-details[name='+name+']')
     var catDetails = $('.cat-details')
 
     var colorImg = $('.cat-img[name='+name+']').find('img.color')
-    var colorImgs = $('.cat-img').find('img.color')
+    var colorImgsAll = $('.cat-img').find('img.color')
+
+    var catImgsHere = photoBox.find('.cat-img')
 
     var wasHidden = catDetail.css('display') == 'none'
 
-    colorImgs.removeClass('active')
+    var scrollOffset = 0
+    for (var i=0; $(catImgsHere[i]).attr('name') != name; i++)
+        scrollOffset += $(catImgsHere[i]).width()
+    // defined variables
+
+    // reset everything
+    colorImgsAll.removeClass('active')
     catDetails.hide()
 
+    // only show if it wasn't showing
     if (wasHidden) {
+        photoBox.scrollLeft(scrollOffset)
         colorImg.addClass('active')
         catDetail.show()
     }
