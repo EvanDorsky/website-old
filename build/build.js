7d86b40b16f3fac0e8663042317f77d0ec589950
Evan Dorsky <evan.dorsky@icloud.com>
2017-03-12 15:45:35 -0700

    Remove "newlines", start node build scheme

---
+++ build/build.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+(function() {
+    const pug = require('pug'),
+    fs = require('fs')
+
+    var basicHandler = function(err, data) {
+        if (err) console.error(err)
+    }
+
+    module.exports = {
+        renderIndex: function() {
+            fs.readFile('js/coolstuff.json', 'utf8', function(err, data) {
+                basicHandler(err, data)
+
+                var indexPage = pug.compileFile('pug/index.pug')
+
+                fs.writeFile('index.html', indexPage(JSON.parse(data)), basicHandler)
+            })
+        },
+        renderCats: function() {
+            fs.readFile('js/coolstuff.json', 'utf8', function(err, data) {
+                data = JSON.parse(data)
+
+                for (cat in data.categories) {
+                    console.log(cat)
+
+                    var catPage = pug.compileFile('pug/layout.pug')
+
+                    fs.writeFile(cat+'.html', catPage({
+                        poke: data.categories[cat]
+                    }), basicHandler)
+                }
+            })
+        }
+    }
+
+    if (!module.parent) {
+        var func = process.argv[2]
+        if (func in module.exports)
+            module.exports[process.argv[2]]()
+        else
+            console.error('Invalid call')
+    }
+})()
