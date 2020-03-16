f8cef3ca8731e58007d4d408b757d493f038c268
Evan Dorsky <evan.dorsky@icloud.com>
2020-03-15 18:02:44 -0700

    New content, "open" image support (for shelf)

--- js/main.js
+++ js/main.js
@@ -16,6 +16,92 @@ function init() {
     }
 }
 
+function handleDetailDisplay(detail) {
+    // this breaks courses
+    detail.insertBefore($(detail).parent().find('div:first-child'))
+
+    detail.show()
+    detail.css({'height': 'auto'})
+    catHeight = detail.height()
+    detail.css({
+        'height': 0,
+        'transition': 'padding '+focusTime+'ms, opacity '+focusTime+'ms, height '+focusTime+'ms'
+    })
+    setTimeout(function() {
+        detail.addClass('active')
+        detail.css({
+            'height': catHeight + 16
+        })
+    }, 5)
+}
+
+function handleImgClick() {
+    var name = $(this).attr('name')
+    var photoBox = $(this).parent()
+    var catImg = $(this)
+
+    var catDetail = $('.cat-details[name='+name+']')
+    var catAll = $('.cat-details')
+    var catDetails = $('.cat-details')
+
+    var colorImg = $('.cat-img[name='+name+']').find('img.color')
+    var openImg = $('.cat-img[name='+name+']').find('img.open')
+    var colorImgsAll = $('.cat-img').find('img.color')
+    var openImgsAll = $('.cat-img').find('img.open')
+
+    var wasHidden = !colorImg.hasClass('active')
+
+    if (wasHidden)
+        catDetails = catDetails.filter(function(i) {
+            return $(catDetails[i]).attr('name') != name
+        })
+
+    var catTitlesAll = $('.cat-title')
+
+    var catImgsHere = photoBox.find('.cat-img')
+
+    var whiteOuts = $('.whiteout')
+    var whiteOut = catImg.find('.whiteout')
+
+    var scrollOffset = 0
+    for (var i=0; $(catImgsHere[i]).attr('name') != name; i++)
+        scrollOffset += $(catImgsHere[i]).width()
+    scrollOffset -= photoBox.scrollLeft()
+    // defined variables
+
+    // reset everything
+    whiteOuts.removeClass('opaque')
+    colorImgsAll.removeClass('active')
+    openImgsAll.removeClass('active')
+    catTitlesAll.removeClass('active')
+    catDetails.removeClass('active')
+    catDetails.css({'height': 0})
+    
+    clearTimeout(hideDetails)
+    hideDetails = setTimeout(function() {
+        catDetails.hide()
+    }, focusTime)
+
+    // only show if it wasn't showing -- and isn't a course
+    if (wasHidden && !photoBox.parent().hasClass('courses')) {
+        whiteOuts.addClass('opaque')
+        whiteOut.removeClass('opaque')
+        catTitlesAll.addClass('active')
+        $('html').velocity('scroll', {
+            container: photoBox,
+            axis: 'x',
+            duration: 1.2*Math.abs(scrollOffset),
+            offset: scrollOffset
+        })
+        if (openImg) {
+          openImg.addClass('active')
+        }
+        colorImg.addClass('active')
+
+        handleDetailDisplay(catDetail)
+    }
+}
+
 function add(x, y) {
     return x+y
 }
@@ -66,84 +152,4 @@ function lastChildMargin() {
         lastCat.css({
             'margin-right': 0
         })
-}
-
-function handleDetailDisplay(detail) {
-    // this breaks courses
-    detail.insertBefore($(detail).parent().find('div:first-child'))
-
-    detail.show()
-    detail.css({'height': 'auto'})
-    catHeight = detail.height()
-    detail.css({
-        'height': 0,
-        'transition': 'padding '+focusTime+'ms, opacity '+focusTime+'ms, height '+focusTime+'ms'
-    })
-    setTimeout(function() {
-        detail.addClass('active')
-        detail.css({
-            'height': catHeight + 16
-        })
-    }, 5)
-}
-
-function handleImgClick() {
-    var name = $(this).attr('name')
-    var photoBox = $(this).parent()
-    var catImg = $(this)
-
-    var catDetail = $('.cat-details[name='+name+']')
-    var catAll = $('.cat-details')
-    var catDetails = $('.cat-details')
-
-    var colorImg = $('.cat-img[name='+name+']').find('img.color')
-    var colorImgsAll = $('.cat-img').find('img.color')
-
-    var wasHidden = !colorImg.hasClass('active')
-
-    if (wasHidden)
-        catDetails = catDetails.filter(function(i) {
-            return $(catDetails[i]).attr('name') != name
-        })
-
-    var catTitlesAll = $('.cat-title')
-
-    var catImgsHere = photoBox.find('.cat-img')
-
-    var whiteOuts = $('.whiteout')
-    var whiteOut = catImg.find('.whiteout')
-
-    var scrollOffset = 0
-    for (var i=0; $(catImgsHere[i]).attr('name') != name; i++)
-        scrollOffset += $(catImgsHere[i]).width()
-    scrollOffset -= photoBox.scrollLeft()
-    // defined variables
-
-    // reset everything
-    whiteOuts.removeClass('opaque')
-    colorImgsAll.removeClass('active')
-    catTitlesAll.removeClass('active')
-    catDetails.removeClass('active')
-    catDetails.css({'height': 0})
-    
-    clearTimeout(hideDetails)
-    hideDetails = setTimeout(function() {
-        catDetails.hide()
-    }, focusTime)
-
-    // only show if it wasn't showing -- and isn't a course
-    if (wasHidden && !photoBox.parent().hasClass('courses')) {
-        whiteOuts.addClass('opaque')
-        whiteOut.removeClass('opaque')
-        catTitlesAll.addClass('active')
-        $('html').velocity('scroll', {
-            container: photoBox,
-            axis: 'x',
-            duration: 1.2*Math.abs(scrollOffset),
-            offset: scrollOffset
-        })
-        colorImg.addClass('active')
-
-        handleDetailDisplay(catDetail)
-    }
 }
